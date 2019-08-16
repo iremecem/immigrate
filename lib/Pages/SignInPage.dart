9ca971e73469d7aeb8e96be4fb93f658a0dfb1b8
Subfly <alitahasubfly@gmail.com>
2019-08-16 22:38:29 +0300

    Leles are fixed

--- lib/Pages/SignInPage.dart
+++ lib/Pages/SignInPage.dart
@@ -23,140 +23,139 @@ class _SignInPageState extends State<SignInPage> {
     return Scaffold(
       resizeToAvoidBottomPadding: true,
       resizeToAvoidBottomInset: true,
-      body: Padding(
-        padding: EdgeInsets.all(12),
-        child: Column(
-          children: <Widget>[
-            Container(
-              height: 30,
-            ),
-            Image(
-              image: AssetImage("assets/images/dummy-imm.png"),
-              height: 300,
-              width: 300,
-            ),
-            Container(
-              height: 30,
-            ),
-            SDCard(
-              title: "Sign Up to Countryman App",
-              content: Center(
-                child: FormBuilder(
-                  key: _fbKey,
-                  autovalidate: false,
-                  child: Column(
-                    children: <Widget>[
-                      FormBuilderTextField(
-                        attribute: "email",
-                        validators: [
-                          FormBuilderValidators.email(),
-                          FormBuilderValidators.required(),
-                        ],
-                        controller: _mailController,
-                        decoration: InputDecoration(
-                          labelText: "Email",
-                          hintText: "example@anymail.com",
-                          border: InputBorder.none,
-                          labelStyle: TextStyle(color: Colors.lightGreen),
-                          icon: Icon(
-                            FontAwesomeIcons.envelope,
-                            color: Colors.lightGreen,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(12),
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 30,
+              ),
+              Image(
+                image: AssetImage("assets/images/dummy-imm.png"),
+                height: 300,
+                width: 300,
+              ),
+              Container(
+                height: 30,
+              ),
+              SDCard(
+                title: "Sign Up to Countryman App",
+                content: Center(
+                  child: FormBuilder(
+                    key: _fbKey,
+                    autovalidate: false,
+                    child: Column(
+                      children: <Widget>[
+                        FormBuilderTextField(
+                          attribute: "email",
+                          validators: [
+                            FormBuilderValidators.email(),
+                            FormBuilderValidators.required(),
+                          ],
+                          controller: _mailController,
+                          decoration: InputDecoration(
+                            labelText: "Email",
+                            hintText: "example@anymail.com",
+                            border: InputBorder.none,
+                            labelStyle: TextStyle(color: Colors.lightGreen),
+                            icon: Icon(
+                              FontAwesomeIcons.envelope,
+                              color: Colors.lightGreen,
+                            ),
                           ),
                         ),
-                      ),
-                      FormBuilderTextField(
-                        attribute: "password",
-                        validators: [
-                          FormBuilderValidators.minLength(6),
-                          FormBuilderValidators.required(),
-                        ],
-                        obscureText: true,
-                        controller: _passwordController,
-                        decoration: InputDecoration(
-                          labelText: "Password",
-                          labelStyle: TextStyle(color: Colors.lightGreen),
-                          border: InputBorder.none,
-                          icon: Icon(
-                            FontAwesomeIcons.unlockAlt,
-                            color: Colors.lightGreen,
+                        FormBuilderTextField(
+                          attribute: "password",
+                          validators: [
+                            FormBuilderValidators.minLength(6),
+                            FormBuilderValidators.required(),
+                          ],
+                          obscureText: true,
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                            labelText: "Password",
+                            labelStyle: TextStyle(color: Colors.lightGreen),
+                            border: InputBorder.none,
+                            icon: Icon(
+                              FontAwesomeIcons.unlockAlt,
+                              color: Colors.lightGreen,
+                            ),
                           ),
                         ),
-                      ),
-                      FormBuilderTextField(
-                        attribute: "password",
-                        validators: [
-                          FormBuilderValidators.minLength(6),
-                          FormBuilderValidators.required(),
-                        ],
-                        obscureText: true,
-                        controller: _secondaryController,
-                        decoration: InputDecoration(
-                          labelText: "Password Again...",
-                          labelStyle: TextStyle(color: Colors.lightGreen),
-                          border: InputBorder.none,
-                          icon: Icon(
-                            FontAwesomeIcons.unlockAlt,
-                            color: Colors.lightGreen,
+                        FormBuilderTextField(
+                          attribute: "password",
+                          validators: [
+                            FormBuilderValidators.minLength(6),
+                            FormBuilderValidators.required(),
+                          ],
+                          obscureText: true,
+                          controller: _secondaryController,
+                          decoration: InputDecoration(
+                            labelText: "Password Again...",
+                            labelStyle: TextStyle(color: Colors.lightGreen),
+                            border: InputBorder.none,
+                            icon: Icon(
+                              FontAwesomeIcons.unlockAlt,
+                              color: Colors.lightGreen,
+                            ),
                           ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              height: 25,
-            ),
-            FlatButton(
-              onPressed: () async {
-                _fbKey.currentState.save();
-                if (_fbKey.currentState.validate() &&
-                    _passwordController.text == _secondaryController.text) {
-                  print(_fbKey.currentState.value);
-                  await _controller.authUser(
-                    ctx: context,
-                    email: _mailController.text.trim(),
-                    password: _passwordController.text.trim(),
-                  );
-                } else {
-                  Flushbar(
-                    backgroundColor: Colors.red,
-                    flushbarStyle: FlushbarStyle.FLOATING,
-                    flushbarPosition: FlushbarPosition.BOTTOM,
-                    message:
-                        "Passwords are not same or empty, please re-enter!",
-                    isDismissible: true,
-                    duration: Duration(seconds: 5),
-                  )..show(context);
-                }
-              },
-              child: Text(
-                "Sign Up",
-                style: TextStyle(
-                  color: Colors.lightGreen,
-                  fontSize: 20
+              Container(
+                height: 25,
+              ),
+              FlatButton(
+                onPressed: () async {
+                  _fbKey.currentState.save();
+                  if (_fbKey.currentState.validate() &&
+                      _passwordController.text == _secondaryController.text) {
+                    print(_fbKey.currentState.value);
+                    await _controller.authUser(
+                      ctx: context,
+                      email: _mailController.text.trim(),
+                      password: _passwordController.text.trim(),
+                    );
+                  } else {
+                    Flushbar(
+                      backgroundColor: Colors.red,
+                      flushbarStyle: FlushbarStyle.FLOATING,
+                      flushbarPosition: FlushbarPosition.BOTTOM,
+                      message:
+                          "Passwords are not same or empty, please re-enter!",
+                      isDismissible: true,
+                      duration: Duration(seconds: 5),
+                    )..show(context);
+                  }
+                },
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(color: Colors.lightGreen, fontSize: 20),
                 ),
               ),
-            ),
-            Container(
-              height: 25,
-            ),
-            Text("Already signed in?"),
-            FlatButton(
-              onPressed: () => Navigator.of(context).pushReplacement(
-                MaterialPageRoute(
-                  builder: (ctx) => LoginPage(),
+              Container(
+                height: 25,
+              ),
+              Text("Already signed in?"),
+              FlatButton(
+                onPressed: () => Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(
+                    builder: (ctx) => LoginPage(),
+                  ),
+                ),
+                child: Text(
+                  "Sign In",
+                  style: TextStyle(
+                    color: Colors.lightGreen,
+                  ),
                 ),
               ),
-              child: Text(
-                "Sign In",
-                style: TextStyle(
-                  color: Colors.lightGreen,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
